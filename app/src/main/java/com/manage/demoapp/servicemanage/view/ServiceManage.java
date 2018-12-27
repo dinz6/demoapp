fff5fc99b4ad97dc955052c79abd1ed0739448bd
cdchen <cdchen@iflytek.com>
2018-12-27 19:27:21 +0800

    服务管理初版

--- app/src/main/java/com/manage/demoapp/servicemanage/view/ServiceManage.java
+++ app/src/main/java/com/manage/demoapp/servicemanage/view/ServiceManage.java
@@ -2,6 +2,8 @@ package com.manage.demoapp.servicemanage.view;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
@@ -9,20 +11,57 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import cn.qqtheme.framework.picker.LinkagePicker;
 import cn.qqtheme.framework.picker.OptionPicker;
 import com.manage.demoapp.R;
 import com.manage.demoapp.helper.PickerHelper;
 import com.manage.demoapp.servicemanage.model.ServiceAdapter;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class ServiceManage extends AppCompatActivity {
     @BindView(R.id.serviceManage_back)
     ImageView back;
     @BindView(R.id.serviceManage_list)
     ListView listView;
+    @BindView(R.id.serviceManage_type)
+    ImageView category;
     ServiceAdapter serviceAdapter;
-    private String[] itemOptions = {"编辑", "新增"};
+    private String[] itemOptions = {"编辑"};
 
 
+    final LinkagePicker.DataProvider dataProvider = new LinkagePicker.DataProvider() {
+        @NonNull
+        @Override
+        public List<String> provideFirstData() {
+            return Arrays.asList("线上", "线下");
+        }
+
+        @NonNull
+        @Override
+        public List<String> provideSecondData(int firstIndex) {
+            switch (firstIndex) {
+                case 0:
+                    return Arrays.asList("1", "2");
+                case 1:
+                    return Arrays.asList("3", "4");
+                default:
+                    return Arrays.asList("5", "6");
+            }
+        }
+
+        @Nullable
+        @Override
+        public List<String> provideThirdData(int firstIndex, int secondIndex) {
+            return null;
+        }
+
+        @Override
+        public boolean isOnlyTwo() {
+            return true;
+        }
+    };
     private Activity mContext = ServiceManage.this;
     private OptionPicker picker;
 
@@ -43,6 +82,19 @@ public class ServiceManage extends AppCompatActivity {
 
                     }
                 });
+            }
+        });
+
+        bindEvents();
+    }
+
+    private void bindEvents() {
+
+        category.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LinkagePicker linkagePicker = new LinkagePicker(mContext, dataProvider);
+                linkagePicker.show();
             }
         });
     }
