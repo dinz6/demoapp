362172946b6a8386bb09f59b7e62ad2479016296
Peter-WP <34291834+Peter-WP@users.noreply.github.com>
2018-12-29 01:19:51 +0800

    1、合并部分文件

--- app/src/main/java/com/manage/demoapp/index/view/Index.java
+++ app/src/main/java/com/manage/demoapp/index/view/Index.java
@@ -3,17 +3,17 @@ package com.manage.demoapp.index.view;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.LinearLayout;
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
 
 import com.manage.demoapp.R;
 import com.manage.demoapp.organizationmanage.ui.OrganizationActivity;
 import com.manage.demoapp.servicemanage.view.ServiceManage;
 import com.manage.demoapp.staffmanage.view.StaffManage;
 import com.manage.demoapp.workordermanage.view.WorkOrderManage;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class Index extends AppCompatActivity {
     @BindView(R.id.index_order)
@@ -31,26 +31,11 @@ public class Index extends AppCompatActivity {
         setContentView(R.layout.activity_index);
         ButterKnife.bind(this);
 
-        order.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(Index.this, WorkOrderManage.class));
-            }
-        });
-        service.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(Index.this, ServiceManage.class));
-            }
-        });
-    }
-        staff.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(Index.this, StaffManage.class));
-            }
-        });
+        order.setOnClickListener(v -> startActivity(new Intent(Index.this, WorkOrderManage.class)));
+        service.setOnClickListener(v -> startActivity(new Intent(Index.this, ServiceManage.class)));
 
+        staff.setOnClickListener(v -> startActivity(new Intent(Index.this, StaffManage.class)));
+    }
     @OnClick(R.id.index_org)
     public void orgClick(){
         startActivity(new Intent(this,OrganizationActivity.class));
