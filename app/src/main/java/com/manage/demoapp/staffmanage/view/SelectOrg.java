663cdcacabb3410c83f462fa4ff68794f7e5cf46
cdchen <cdchen@iflytek.com>
2018-12-28 16:38:19 +0800

    派单选择

--- app/src/main/java/com/manage/demoapp/staffmanage/view/SelectOrg.java
+++ app/src/main/java/com/manage/demoapp/staffmanage/view/SelectOrg.java
@@ -3,6 +3,7 @@ package com.manage.demoapp.staffmanage.view;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.ListView;
 import butterknife.BindView;
@@ -10,6 +11,8 @@ import butterknife.ButterKnife;
 import com.manage.demoapp.R;
 import com.manage.demoapp.staffmanage.model.OrgAdapter;
 import com.manage.demoapp.staffmanage.model.OrgConstants;
+import com.manage.demoapp.staffmanage.model.SimpleStaffAdapter;
+import com.manage.demoapp.staffmanage.model.StaffConstants;
 import org.angmarch.views.NiceSpinner;
 
 import java.util.Arrays;
@@ -21,10 +24,13 @@ public class SelectOrg extends AppCompatActivity {
     NiceSpinner niceSpinner;
 
     private OrgAdapter orgAdapter;
+    private SimpleStaffAdapter simpleStaffAdapter;
     @BindView(R.id.selectOrg_list)
     ListView listView;
     @BindView(R.id.selectOrder_back)
     ImageView back;
+
+    private boolean flag = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,17 +38,49 @@ public class SelectOrg extends AppCompatActivity {
         setContentView(R.layout.activity_select_org);
         ButterKnife.bind(this);
 
-        List<String> dataset = new LinkedList<>(Arrays.asList("机构名称", "机构编码"));
-        niceSpinner.attachDataSource(dataset);
 
         orgAdapter = new OrgAdapter(OrgConstants.orgs(), this);
 
         listView.setAdapter(orgAdapter);
+        rebind(listView);
+
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                finish();
+                if (flag) {
+                    finish();
+                } else {
+                    listView.setAdapter(orgAdapter);
+                    flag = true;
+                    rebind(listView);
+                    List<String> dataset = new LinkedList<>(Arrays.asList("机构名称", "机构编码"));
+                    niceSpinner.attachDataSource(dataset);
+                }
             }
         });
+    }
+
+    private void rebind(final ListView listView) {
+        if (flag) {
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    simpleStaffAdapter = new SimpleStaffAdapter(StaffConstants.simpleStaff(), SelectOrg.this);
+                    flag = false;
+                    listView.setAdapter(simpleStaffAdapter);
+                    rebind(listView);
+                    List<String> dataset = new LinkedList<>(Arrays.asList("姓名", "工号"));
+                    niceSpinner.attachDataSource(dataset);
+                }
+            });
+
+        } else {
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                }
+            });
+        }
     }
 }
